f77f590f60c50771e496c1661a3a44cd5d9792a1
Akshat Gupta <akshatgtc@gmail.com>
2023-08-28 18:39:31 +0530

    setup login logout

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
  import NextAuth from "next-auth/next";
  import GoogleProvider from 'next-auth/providers/google';
+ import User from "@models/user";
  import {connectToDB} from '@utils/database'
 
 
@@ -16,10 +17,19 @@
    async signIn({profile}){
     try {
         await connectToDB();
+        const userExists = await User.findOne({email: profile.email});
+        if(!userExists){
+            await User.create({
+                email: profile.email,
+                username: profile.name.replace(" ","").toLowerCase(),
+                image: profile.picture
+            })
+        }
+        
         return true;
     } catch (error) {
         console.log(error);
-        return false;
+        return false;     
     }
    }
  })
